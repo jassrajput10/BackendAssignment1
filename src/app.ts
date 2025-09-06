5f9a5690ec797d73f144c5b602529b493c90eea8
jparmar10 <jparmar190@rrc.ca>
2025-09-06 17:14:00 -0500

    Development Environment Setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// import the express application and type definition
+import express, { Express, Request, Response } from "express";
+
+// initialize Express application
+const app: Express = express();
+
+// Define basic routes
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, World!");
+});
+
+export default app;
